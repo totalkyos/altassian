97e125369aa09f0904f8b768a2d24e0da25ce56f
Juan Palacios <jpalacios@atlassian.com>
2014-10-03 14:12:16 +1000

    Moved the logic to read and write a collection of MemberRoles to the MemberRole enum

--- hazelcast/src/main/java/com/hazelcast/instance/MemberRole.java
+++ hazelcast/src/main/java/com/hazelcast/instance/MemberRole.java
@@ -1,5 +1,12 @@
 package com.hazelcast.instance;
 
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.Set;
+
 /**
  * Defines the different roles a member can have in the cluster
  * A member with no roles has read and write access to the data in the cluster
@@ -22,10 +29,6 @@ public enum MemberRole {
         this.id = id;
     }
 
-    public int getId() {
-        return id;
-    }
-
     public static MemberRole valueOf(int id) {
         for (MemberRole role : values()) {
             if (role.id == id) {
@@ -33,5 +36,21 @@ public enum MemberRole {
             }
         }
         throw new IllegalArgumentException(String.format("No MemberRole with id [%d] could be found", id));
+    }
+
+    public static Set<MemberRole> readRoles(ObjectDataInput in) throws IOException {
+        int size = in.readInt();
+        EnumSet<MemberRole> roles = EnumSet.noneOf(MemberRole.class);
+        for (int i = 0; i < size; i++) {
+            roles.add(valueOf(in.readInt()));
+        }
+        return roles;
+    }
+
+    public static void writeRoles(ObjectDataOutput out, Set<MemberRole> roles) throws IOException {
+        out.writeInt(roles.size());
+        for (MemberRole role : roles) {
+            out.writeInt(role.id);
+        }
     }
 }
