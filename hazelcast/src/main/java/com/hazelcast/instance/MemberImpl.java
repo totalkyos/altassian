0289a4ec85d93876a7905d77ddd88a4e58663f4b
Chris Szmajda <cszmajda@atlassian.com>
2015-06-24 14:38:11 +1000

    Removed unnecessary code after merging, fixing MembershipListenerTest

--- hazelcast/src/main/java/com/hazelcast/instance/MemberImpl.java
+++ hazelcast/src/main/java/com/hazelcast/instance/MemberImpl.java
@@ -361,47 +361,4 @@ public final class MemberImpl
         return capabilities.contains(capability);
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Member [");
-        sb.append(address.getHost());
-        sb.append("]");
-        sb.append(":");
-        sb.append(address.getPort());
-        if (localMember) {
-            sb.append(" this");
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final MemberImpl other = (MemberImpl) obj;
-        if (address == null) {
-            if (other.address != null) {
-                return false;
-            }
-        } else if (!address.equals(other.address)) {
-            return false;
-        }
-        return true;
-    }
-
 }
